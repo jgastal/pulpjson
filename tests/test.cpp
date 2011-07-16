0f68484f168d74e68df6f303dc9dc89a66814044
Jonas M. Gastal <jgastal@gmail.com>
2011-07-15 23:04:06 -0300

    More tests.

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,17 +5,6 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
-
-
-
-// #include <iostream>
-// 	for(Object::iterator it = obj->begin(); it != obj->end(); it++)
-// 		std::cout << "key: \"" <<  (*it).first << "\"" << std::endl;
-
-
-
-
-
 using namespace libjson;
 
 BOOST_AUTO_TEST_CASE( empty )
@@ -104,7 +93,8 @@ BOOST_AUTO_TEST_CASE( double_prop )
 
 BOOST_AUTO_TEST_CASE( object_prop )
 {
-	Parser p("{ a: {}, b: {a: {a: { a: {a: 1234, b: \"asdf\", c: true, d:false, e: 0.7}}}}, c: {a: 1234, b: \"asdf\", c: true, d:false, e: 0.654} }", false);
+	Parser p("{ a: {}, b: {a: {a: { a: {a: 1234, b: \"asdf\", c: true, d:false, e: 0.7}}}}, c: "
+		 "{a: 1234, b: \"asdf\", c: true, d:false, e: 0.654} }", false);
 	Object *obj = p.getRootObject();
 
 	BOOST_REQUIRE_EQUAL(obj->size(), 3);
@@ -147,4 +137,81 @@ BOOST_AUTO_TEST_CASE( object_prop )
 	BOOST_REQUIRE_EQUAL((*c)["c"]->asBool(), true);
 	BOOST_REQUIRE_EQUAL((*c)["d"]->asBool(), false);
 	BOOST_REQUIRE_EQUAL((*c)["e"]->asDouble(), 0.654);
+}
+
+BOOST_AUTO_TEST_CASE( vector_prop )
+{
+	Parser p("{ a: [], b: [1, 2, 3], c: [true, false, true], d: [0.5, 4.235, 78.654],"
+		 "e: [[1, 2, 3], {a: [[]]}], f: [\"asdf\", 1, {a: []}]", false);
+	Object *obj = p.getRootObject();
+
+	BOOST_REQUIRE_EQUAL(obj->size(), 6);
+	BOOST_REQUIRE_EQUAL((*obj)["a"]->getType(), Value::VECTOR);
+	BOOST_CHECK_EQUAL((*obj)["a"]->asVector()->size(), 0);
+
+	BOOST_REQUIRE_EQUAL((*obj)["b"]->getType(), Value::VECTOR);
+	Vector *b = (*obj)["b"]->asVector();
+	BOOST_REQUIRE_EQUAL(b->size(), 3);
+	BOOST_REQUIRE_EQUAL((*b)[0]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*b)[0]->asInt(), 1);
+	BOOST_REQUIRE_EQUAL((*b)[1]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*b)[1]->asInt(), 2);
+	BOOST_REQUIRE_EQUAL((*b)[2]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*b)[2]->asInt(), 3);
+
+	BOOST_REQUIRE_EQUAL((*obj)["c"]->getType(), Value::VECTOR);
+	Vector *c = (*obj)["c"]->asVector();
+	BOOST_REQUIRE_EQUAL(c->size(), 3);
+	BOOST_REQUIRE_EQUAL((*c)[0]->getType(), Value::BOOL);
+	BOOST_CHECK_EQUAL((*c)[0]->asBool(), true);
+	BOOST_REQUIRE_EQUAL((*c)[1]->getType(), Value::BOOL);
+	BOOST_CHECK_EQUAL((*c)[1]->asBool(), false);
+	BOOST_REQUIRE_EQUAL((*c)[2]->getType(), Value::BOOL);
+	BOOST_CHECK_EQUAL((*c)[2]->asBool(), true);
+
+	BOOST_REQUIRE_EQUAL((*obj)["d"]->getType(), Value::VECTOR);
+	Vector *d = (*obj)["d"]->asVector();
+	BOOST_REQUIRE_EQUAL(d->size(), 3);
+	BOOST_REQUIRE_EQUAL((*d)[0]->getType(), Value::DOUBLE);
+	BOOST_CHECK_EQUAL((*d)[0]->asDouble(), 0.5);
+	BOOST_REQUIRE_EQUAL((*d)[1]->getType(), Value::DOUBLE);
+	BOOST_CHECK_EQUAL((*d)[1]->asDouble(), 4.235);
+	BOOST_REQUIRE_EQUAL((*d)[2]->getType(), Value::DOUBLE);
+	BOOST_CHECK_EQUAL((*d)[2]->asDouble(), 78.654);
+
+	BOOST_REQUIRE_EQUAL((*obj)["e"]->getType(), Value::VECTOR);
+	Vector *e = (*obj)["e"]->asVector();
+	BOOST_REQUIRE_EQUAL(e->size(), 2);
+	BOOST_REQUIRE_EQUAL((*e)[0]->getType(), Value::VECTOR);
+	Vector *e1 = (*e)[0]->asVector();
+	BOOST_REQUIRE_EQUAL(e1->size(), 3);
+	BOOST_REQUIRE_EQUAL((*e1)[0]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*e1)[0]->asInt(), 1);
+	BOOST_REQUIRE_EQUAL((*e1)[1]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*e1)[1]->asInt(), 2);
+	BOOST_REQUIRE_EQUAL((*e1)[2]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*e1)[2]->asInt(), 3);
+
+	BOOST_REQUIRE_EQUAL((*e)[1]->getType(), Value::OBJECT);
+	Object *e2 = (*e)[1]->asObject();
+	BOOST_REQUIRE_EQUAL(e2->size(), 1);
+	BOOST_REQUIRE_EQUAL((*e2)["a"]->getType(), Value::VECTOR);
+	Vector *e2a = (*e2)["a"]->asVector();
+	BOOST_REQUIRE_EQUAL(e2a->size(), 1);
+	BOOST_REQUIRE_EQUAL((*e2a)[0]->getType(), Value::VECTOR);
+	Vector *e2a1 = (*e2a)[0]->asVector();
+	BOOST_REQUIRE_EQUAL(e2a1->size(), 0);
+
+	BOOST_REQUIRE_EQUAL((*obj)["f"]->getType(), Value::VECTOR);
+	Vector *f = (*obj)["f"]->asVector();
+	BOOST_REQUIRE_EQUAL(f->size(), 3);
+	BOOST_REQUIRE_EQUAL((*f)[0]->getType(), Value::STRING);
+	BOOST_CHECK_EQUAL((*f)[0]->asString().compare("asdf"), 0);
+	BOOST_REQUIRE_EQUAL((*f)[1]->getType(), Value::INT);
+	BOOST_CHECK_EQUAL((*f)[1]->asInt(), 1);
+	BOOST_REQUIRE_EQUAL((*f)[2]->getType(), Value::OBJECT);
+	Object *f3  = (*f)[2]->asObject();
+	BOOST_CHECK_EQUAL(f3->size(), 1);
+	BOOST_REQUIRE_EQUAL((*f3)["a"]->getType(), Value::VECTOR);
+	BOOST_REQUIRE_EQUAL((*f3)["a"]->asVector()->size(), 0);
 }
